54fb282c51c4c661c79e50643a9e295ddc189228
Daniel <pikachungg@gmail.com>
2021-06-20 02:10:23 -0500

    fixed overflow bug

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 // Some random colors
 const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
 
-const numBalls = 90;
+
+const numBalls = 50;
 const balls = [];
 
 for (let i = 0; i < numBalls; i++) {
   let ball = document.createElement("div");
   ball.classList.add("ball");
   ball.style.background = colors[Math.floor(Math.random() * colors.length)];
-  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+  ball.style.left = `${Math.floor(Math.random() * 80)}vw`;
   ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
   ball.style.transform = `scale(${Math.random()})`;
   ball.style.width = `${Math.random()}em`;
@@ -21,7 +22,7 @@ for (let i = 0; i < numBalls; i++) {
 // Keyframes
 balls.forEach((el, i, ra) => {
   let to = {
-    x: Math.random() * (i % 2 === 0 ? -11 : 11),
+    x: Math.random() * (i % 2 === 0 ? -3 : 3),
     y: Math.random() * 12
   };
 
